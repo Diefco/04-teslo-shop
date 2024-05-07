22c70048b744de3c874dd36abaed844dd22a0aa0
Diego Díaz <diefco.web@gmail.com>
2024-05-06 19:57:20 -0500

    Create product in service

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  private readonly logger = new Logger('ProductsService');
+
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      if (!createProductDto.slug) {
+        createProductDto.slug = createProductDto.title
+          .toLowerCase()
+          .replaceAll(/ /g, '_')
+          .replaceAll("'", '');
+      } else {
+        createProductDto.slug = createProductDto.slug
+          .toLowerCase()
+          .replaceAll(/ /g, '_')
+          .replaceAll("'", '');
+      }
+
+      const product = this.productRepository.create(createProductDto);
+      await this.productRepository.save(product);
+
+      return product;
+    } catch (error) {
+      this.handleDBExepction(error);
+    }
+    return;
   }
 
   findAll() {
@@ -22,5 +57,16 @@ export class ProductsService {
 
   remove(id: number) {
     return `This action removes a #${id} product`;
+  }
+
+  private handleDBExepction(error: any) {
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    }
+
+    this.logger.error(error);
+    throw new InternalServerErrorException(
+      'Unexpected error, check server logs for more details.',
+    );
   }
 }
