1c2ce319d8fff1961e1739316bcd9f4ec0963070
Diego Díaz <diefco.web@gmail.com>
2024-05-06 21:26:44 -0500

    pagination for find all products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -10,6 +10,7 @@ import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
 
 @Injectable()
 export class ProductsService {
@@ -32,9 +33,13 @@ export class ProductsService {
     return;
   }
 
-  // TODO: Pagination
-  findAll() {
-    return this.productRepository.find({});
+  findAll(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto;
+    return this.productRepository.find({
+      take: limit,
+      skip: offset,
+      // TODO: Relations
+    });
   }
 
   async findOne(id: string) {
