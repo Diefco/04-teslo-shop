cf185cc4a1e738fa567d5a23d0c44ab78ec16b03
Diego Díaz <diefco.web@gmail.com>
2024-05-13 21:53:13 -0500

    Image upload and validation with helpers

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,7 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { fileFilter, fileNamer } from './helpers';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
+  @Post('product')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+      // limits: { fileSize: 1024 * 1024 },
+      storage: diskStorage({
+        destination: './static/products',
+        filename: fileNamer,
+      }),
+    }),
+  )
+  uploadpProductImage(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException('Make sure that the file is an image');
+    }
+
+    const secureUrl = `${file.filename}`;
+
+    return { secureUrl };
+  }
 }
