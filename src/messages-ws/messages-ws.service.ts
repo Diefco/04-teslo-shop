806e418f0f2399cc36c101a6db9ca0559c9d6881
Diego Díaz <diefco.web@gmail.com>
2024-05-18 01:06:59 -0500

    Add user registration and authentication for WebSocket connections

--- src/messages-ws/messages-ws.service.ts
+++ src/messages-ws/messages-ws.service.ts
@@ -1,15 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { Socket } from 'socket.io';
+import { Repository } from 'typeorm';
+import { User } from 'src/auth/entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 interface ConnectedClients {
-  [id: string]: Socket;
+  [id: string]: {
+    socket: Socket;
+    user: User;
+    // desktop: boolean;
+  };
 }
 @Injectable()
 export class MessagesWsService {
   private connectedClients: ConnectedClients = {};
 
-  registerClient(client: Socket) {
-    this.connectedClients[client.id] = client;
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async registerClient(client: Socket, userId: string) {
+    const user = await this.userRepository.findOneBy({ id: userId });
+
+    if (!user) throw new Error('User not found');
+    if (!user.isActive) throw new Error('User is not active');
+
+    this.checkUserConnection(user);
+
+    this.connectedClients[client.id] = { socket: client, user: user };
   }
 
   removeClient(clientID: string) {
@@ -18,5 +37,19 @@ export class MessagesWsService {
 
   getConnectedClients(): string[] {
     return Object.keys(this.connectedClients);
+  }
+
+  getUserFullName(socketId: string) {
+    return this.connectedClients[socketId].user.fullName;
+  }
+
+  private checkUserConnection(user: User) {
+    for (const clientId of Object.keys(this.connectedClients)) {
+      const connectedClient = this.connectedClients[clientId];
+      if (connectedClient.user.id === user.id) {
+        connectedClient.socket.disconnect();
+        break;
+      }
+    }
   }
 }
