a2169ea3e9fda9547f4ef240460e0f464f52abe4
Diego Díaz <diefco.web@gmail.com>
2024-05-17 20:33:44 -0500

    Register and remove clients in MessagesWsGateway

--- src/messages-ws/messages-ws.service.ts
+++ src/messages-ws/messages-ws.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { Socket } from 'socket.io';
 
+interface ConnectedClients {
+  [id: string]: Socket;
+}
 @Injectable()
-export class MessagesWsService {}
+export class MessagesWsService {
+  private connectedClients: ConnectedClients = {};
+
+  registerClient(client: Socket) {
+    this.connectedClients[client.id] = client;
+  }
+
+  removeClient(clientID: string) {
+    delete this.connectedClients[clientID];
+  }
+
+  getConnectedClients(): string[] {
+    return Object.keys(this.connectedClients);
+  }
+}
