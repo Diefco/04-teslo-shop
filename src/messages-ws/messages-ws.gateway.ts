089664f4a882387178c3ae17c3dcaf1577746df8
Diego Díaz <diefco.web@gmail.com>
2024-05-17 19:47:17 -0500

    Add WebSocketGateway implementation for handling connections and disconnections

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,7 +1,21 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 import { MessagesWsService } from './messages-ws.service';
 
-@WebSocketGateway()
-export class MessagesWsGateway {
+@WebSocketGateway({ cors: true })
+export class MessagesWsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
   constructor(private readonly messagesWsService: MessagesWsService) {}
+
+  handleConnection(client: Socket) {
+    console.log('Connected user:', client.id);
+  }
+  handleDisconnect(client: Socket) {
+    console.log('Disconnected user:', client.id);
+  }
 }
