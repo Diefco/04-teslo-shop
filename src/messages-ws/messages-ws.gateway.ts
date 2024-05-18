a09fcd60f813234fd53303d53b3b1a4fe7ef150d
Diego Díaz <diefco.web@gmail.com>
2024-05-18 01:05:53 -0500

    Add JWT authentication to WebSocket connections

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,11 +1,15 @@
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { JwtService } from '@nestjs/jwt';
 import { MessagesWsService } from './messages-ws.service';
+import { NewMessageDto } from './dtos/new-message.dto';
+import { JwtPayload } from 'src/auth/interfaces';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway
@@ -13,11 +17,31 @@ export class MessagesWsGateway
 {
   @WebSocketServer() wss: Server;
 
-  constructor(private readonly messagesWsService: MessagesWsService) {}
+  constructor(
+    private readonly messagesWsService: MessagesWsService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-  handleConnection(client: Socket) {
+  async handleConnection(client: Socket) {
+    const token = client.handshake.headers.authentication as string;
+    let payload: JwtPayload;
+
+    try {
+      payload = this.jwtService.verify(token);
+      await this.messagesWsService.registerClient(client, payload.id);
+    } catch (error) {
+      client.disconnect();
+      return;
+    }
+
+    // console.log(payload);
     // console.log('Connected user:', client.id);
-    this.messagesWsService.registerClient(client);
+
+    // Example of joining to a specific room.
+    // client.join('room-1')
+    // client.join(client.email) // Join to a room with the client email
+    // this.wss.to('room-1').emit('message-from-server', {...})
+
     this.wss.emit(
       'clients-updated',
       this.messagesWsService.getConnectedClients(),
